3cd2ab0441b9a3c33cbd5c4dddb0402425ca8cb0
FabianCarrasco <fabian220200@gmail.com>
2023-03-22 13:33:09 -0700

    Added MenuDelHogarCapacitor

---
+++ MenuDelHogarCapacitor/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.menudelhogar.app',
+  appName: 'menudelhogarcapacitor',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
