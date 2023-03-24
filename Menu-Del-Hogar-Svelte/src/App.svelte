fbe3da994b1575ae9c54430a3e684f2d7cac2543
FabianCarrasco <fabian220200@gmail.com>
2023-03-23 22:06:35 -0700

    fixed edamamV2 api endpoint again

--- Menu-Del-Hogar-Svelte/src/App.svelte
+++ Menu-Del-Hogar-Svelte/src/App.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
+    import type { Edamam, EdamamV2 } from "./interfaces/Edamam";
+
+    const ApiRoot = "http://fabiancarrasco.com:3000"
+
+    let search: string = "";
+
+    const searchRecipes = async () => {
+        console.log("Vergaaa!")
+        await fetch(`${ApiRoot}/edamam/v2/${search}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    };
 </script>
 
 <main>
-
+    <h1>Menu del Hogar</h1>
+    <form on:submit|preventDefault={searchRecipes}>
+        <input type="text" placeholder="Search for a recipe" bind:value={search}/>
+        <button type="submit">Search</button>
+    </form>
 </main>
 
 <style>
